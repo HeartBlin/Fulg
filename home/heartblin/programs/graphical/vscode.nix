4a37faaca5251bd12e6bc0dab4ac983fb4b98a52
HeartBlin <heartblin@proton.me>
2024-01-24 22:10:15 +0200

    feat: VSCode

---
+++ home/heartblin/programs/graphical/vscode.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+    enableUpdateCheck = false;
+    userSettings = {
+      "window.titleBarStyle" = "custom";
+      "workbench.iconTheme" = "material-icon-theme";
+
+      # Use 'nil' as the Nix language server
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+    };
+
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      ms-vscode.cpptools
+      pkief.material-icon-theme
+    ];
+  };
+
+  home.packages = [
+    pkgs.nil
+  ];
+}
