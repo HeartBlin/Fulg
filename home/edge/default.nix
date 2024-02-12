c31ecfcb51fcd9353825f13c890b0d2c70507944
HeartBlin <heartblin@proton.me>
2024-02-12 15:04:13 +0000

    chore: Added extensions to Edge

--- home/edge/default.nix
+++ home/edge/default.nix
@@ -6,5 +6,14 @@
     enable = true;
     package = pkgs.microsoft-edge-dev;
 
+    extensions = [
+      { id = "faeadnfmdfamenfhaipofoffijhlnkif"; } # AMOLED Theme
+
+      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # DarkReader
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Ublock
+      { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
+      { id = "hjdoplcnndgiblooccencgcggcoihigg"; } # ToS;Dr
+    ];
   };
 }
