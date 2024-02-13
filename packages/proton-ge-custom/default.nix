d2a6a7fa661f332bcce771de038c77aba0d22b37
HeartBlin <heartblin@proton.me>
2024-02-13 19:15:22 +0000

    chore: Updated Proton-GE

--- packages/proton-ge-custom/default.nix
+++ packages/proton-ge-custom/default.nix
@@ -2,12 +2,12 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton8-31";
+  version = "GE-Proton8-32";
 
   # Get the tar.gz file
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/${pname}/releases/download/${version}/${version}.tar.gz";
-    hash = "sha256-ogxZmso7F0q+VB7/NYTvIiqPt5OCxkoAXhtvNr6KVC4=";
+    hash = "sha256-U+5CvBVWCFQRMtb7+KcaHx9Coj7cXg5V0EJd+e51PaA=";
   };
 
   installPhase = ''
