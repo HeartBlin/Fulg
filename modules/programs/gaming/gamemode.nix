f0a19d4ae1336b8a21c1027aa5c4888caa6fe547
HeartBlin <heartblin@proton.me>
2024-02-14 15:07:12 +0200

    fix: Actually let gamemode set nice values

--- modules/programs/gaming/gamemode.nix
+++ modules/programs/gaming/gamemode.nix
@@ -1,16 +1,25 @@
-{ config, lib, ... }: let
+{ config, lib, pkgs,... }: let
   cfg = config.modules.programs.gamemode;
 in {
     config = lib.mkIf cfg.enable {
-    # Allow games to request optimizations 
+    # Allow games to request optimizations
     programs.gamemode = {
       enable = true;
       enableRenice = true;
       settings.general = {
         desiredgov = "performance";
         softrealtime = "auto";
-        renice = 10;
+        renice = 15;
         inhibit_screensaver = 1;
+      };
+    };
+
+    security.wrappers = {
+      gamemode = {
+        owner = "root";
+        group = "root";
+        source = "${pkgs.gamemode}/bin/gamemoderun";
+        capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
       };
     };
   };
